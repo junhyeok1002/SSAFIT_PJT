e91e94608d462dd535815be14a3a903a0fbccc76
최준혁 <chrisgold@naver.com>
2024-05-16 16:12:08 +0900

    initial commit

--- ssafit/src/main/java/com/ssafy/ssafit/model/dto/User.java
+++ ssafit/src/main/java/com/ssafy/ssafit/model/dto/User.java
@@ -1,5 +1,14 @@
 package com.ssafy.ssafit.model.dto;
 
 public class User {
-
+	private int id;
+	private String password;
+	private String name;
+	private String address;
+	private String favorite;
+	private String done;
+	
+	
 }
+
+
