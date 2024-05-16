ec25161c673b488fb694e12b9262950bdf3798fd
최준혁 <chrisgold@naver.com>
2024-05-16 17:58:09 +0900

    feat(today) : test done

--- ssafit/src/main/java/com/ssafy/ssafit/controller/RoutineRestController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/RoutineRestController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,7 @@ public class RoutineRestController {
 	}
 	
 	@GetMapping("/routine")
-	public ResponseEntity<?> list(){
+	public ResponseEntity<?> readAllRoutine(){
 		List<Routine> routines = routineService.getRoutineList(); // 검색조회
 		
 		if(routines == null || routines.size() == 0) {
@@ -33,4 +35,29 @@ public class RoutineRestController {
 		}
 		return new ResponseEntity<List<Routine>>(routines, HttpStatus.OK);
 	}
+	
+	
+//	[
+//	   "Dumbbell_Curl",
+//	   "Dumbbell_Seated_Curl"
+//	]
+	@PostMapping("/routine")
+	public ResponseEntity<?> createRoutine(@RequestBody List<String> fitnessNames) {
+		List<Fitness> fitnessList = new ArrayList<>();
+		
+        // 폼 데이터로 받은 문자열을 Fitness 열거형(enum)으로 변환하여 리스트에 추가
+        for (String name : fitnessNames) {
+            try {
+                Fitness fitness = Fitness.valueOf(name);
+                fitnessList.add(fitness);
+            } catch (IllegalArgumentException e) {
+                // 열거형(enum)에 해당하는 값이 없을 경우 처리할 내용
+                System.out.println("Invalid fitness name: " + name);
+            }
+        }
+        
+        Routine routine = new Routine(fitnessList);
+        return new ResponseEntity<Routine>(routine, HttpStatus.OK);
+    }
+	
 }
