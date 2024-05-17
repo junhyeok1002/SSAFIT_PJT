b6cc6d2525b6e17babc764c76908838852f43e85
kso <kso8760@daum.net>
2024-05-17 10:14:37 +0900

    mysql update

--- ssafit/src/main/resources/sql__PJT.sql
+++ ssafit/src/main/resources/sql__PJT.sql
@@ -29,22 +29,23 @@ VALUES ("ssafy","1234","김싸피","대전광역시 둔산로 15"),
         
         
 CREATE TABLE review (
-    reivewId INT AUTO_INCREMENT,
+    reviewId INT AUTO_INCREMENT,
+    title VARCHAR(20) NOT NULL,
     userId VARCHAR(20) NOT NULL,
     userName VARCHAR(20) NOT NULL,
-    title VARCHAR(20) NOT NULL,
-    content TEXT NOT NULL,
     createTime TIMESTAMP DEFAULT now(),
+    routineList text not null,
+    content TEXT NOT NULL,
     viewCnt INT DEFAULT 0,
-    PRIMARY KEY (reivewId)
+    PRIMARY KEY (reviewId)
 );
 
-INSERT INTO review (userId, userName, title, content) VALUES
-('user1', '홍길동', '좋은 운동', '이 루틴 정말 좋습니다!'),
-('user2', '김철수', '괜찮아요', '할만한 루틴입니다.'),
-('user3', '이영희', '추천합니다', '친구들에게 추천할 만한 루틴입니다.'),
-('user4', '박민수', '별로입니다', '기대에 미치지 못했습니다.'),
-('user5', '최지은', '훌륭해요', '정말 훌륭한 루틴입니다. 만족합니다.');
+INSERT INTO review (userId, userName, title, content, routineList) VALUES
+('ssafy', '홍길동', '좋은 운동', '이 루틴 정말 좋습니다!','asdf'),
+('ssafy', '김철수', '괜찮아요', '할만한 루틴입니다.','asdf'),
+('ssafy', '이영희', '추천합니다', '친구들에게 추천할 만한 루틴입니다.','asdf'),
+('ssafy1', '박민수', '별로입니다', '기대에 미치지 못했습니다.','asdf'),
+('ssafy1', '최지은', '훌륭해요', '정말 훌륭한 루틴입니다. 만족합니다.','asdf');
 
 select * from review;        
 SELECT * FROM User;
