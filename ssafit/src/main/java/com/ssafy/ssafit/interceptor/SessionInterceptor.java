6d13709945978380b7c93729fd93b6cdf4c45d99
kso <kso8760@daum.net>
2024-05-19 22:19:49 +0900

    interceptor update

--- ssafit/src/main/java/com/ssafy/ssafit/interceptor/SessionInterceptor.java
+++ ssafit/src/main/java/com/ssafy/ssafit/interceptor/SessionInterceptor.java
@@ -11,11 +11,13 @@ import jakarta.servlet.http.HttpSession;
 @Component
 public class SessionInterceptor implements HandlerInterceptor{
 	
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 클라이언트의 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용하는 HTTP 메서드
-                .allowedHeaders("*"); // 허용하는 헤더
-    }
-
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		// 세션에 로그인 정보가 있다면 그대로 진행한다.
+		if (session.getAttribute("login") == null) {
+			return false;
+		}
+		return true;
+	}
 }
